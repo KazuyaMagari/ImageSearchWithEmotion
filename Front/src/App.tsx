2b2c924d8dd92e52af8685958e6452fedbba8752
KazuyaMagari <kazuyamagarifuchi1211@gmail.com>
2025-04-09 20:51:20 -0400

    initial commit

---
+++ Front/src/App.tsx
@@ -0,0 +1,12 @@
+import Navs from "./components/Navs";
+import Result from "./components/Result";
+function App() {
+  return (
+    <>
+      <Navs />
+      <Result />
+    </>
+  );
+}
+
+export default App;
---
+++ Front/src/components/Navs.tsx
@@ -0,0 +1,28 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navs() {
+  return (
+    <Navbar expand="lg" className="bg-dark" style={{ height: "7vh" }}>
+      <Container fluid>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="px-0 mx-0"
+            aria-label="Search"
+            style={{ color: "white", width: "80vw" }}
+          />
+          <Button variant="outline-success" className="me-3 ms-3">
+            Search
+          </Button>
+          <Button>Login</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navs;
